b08aa7614809c6213781849b94e4960a1e4ce9c0
Franky-Boy <153214724+fhade@users.noreply.github.com>
2026-03-23 18:35:43 -0400

    Deployed c99c642 with MkDocs version: 1.6.1

--- site/fix-trailing-slash.js
+++
@@ -1,5 +0,0 @@
-if (window.location.pathname.endsWith('/') === false) {
-  const url = new URL(window.location)
-  url.pathname += '/'
-  window.history.replaceState(null, document.title, url.toString())
-}
